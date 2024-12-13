9951b3696c47c264a8ffdc0cf616d4519f22b627
Ronei Couto <ronei@roneicouto.com>
2024-12-13 19:12:36 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/com/roneicouto/dio_mod3/dio_mod3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.roneicouto.dio_mod3.dio_mod3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                      width: 100,
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
